d5804dc7d3e90cae7ddaefb5b318dd4aafecf894
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-31 17:15:12 -0300

    Update Dog.java

--- src/abstractClasses/Dog.java
+++ src/abstractClasses/Dog.java
@@ -1,5 +1,22 @@
 package abstractClasses;
 
-public class Dog {
+public class Dog extends Animal {
+
+	public Dog(String type, String size, double weight) {
+		super(type, size, weight);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void move(String speed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makenoise() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
